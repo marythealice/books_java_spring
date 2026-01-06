147b5f084e880a4ef3c7d80f88d0b731fffe55ff
malice <nantesmalice@gmail>
2026-01-05 22:20:04 -0400

    Adding put endpoint for books

--- src/main/java/com/luv2code/books/books/controller/BookController.java
+++ src/main/java/com/luv2code/books/books/controller/BookController.java
@@ -52,5 +52,13 @@ public class BookController {
         }
     }
 
-
+    @PutMapping("/{title}")
+    public void updateBook(@PathVariable String title, @RequestBody Book updatedBook) {
+        for(int i=0; i<books.size(); i++){
+            if(books.get(i).getTitle().equalsIgnoreCase(title)){
+                books.set(i, updatedBook);
+                return;
+            }
+        }
+    }
 }
